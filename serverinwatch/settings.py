3465d823ba8811e23d9a54e61f01634f10fdfa55
Rafael Bailón <rafaelbailon@ieee.org>
2015-02-20 20:44:42 +0100

    Add quitsmoking app

--- serverinwatch/settings.py
+++ serverinwatch/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quitsmoking'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -64,6 +65,13 @@ DATABASES = {
         'PASSWORD': os.getenv('PASS', ''), 
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
