8eebd7b8b21769cc5580b81d75fb7901113855b2
Rafael Bailón <rafaelbailon@ieee.org>
2015-02-20 20:26:58 +0100

    Update DB settings

--- serverinwatch/settings.py
+++ serverinwatch/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'serverinwatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('BD', ''),
+        'USER': os.getenv('USERBD', ''),
+        'PASSWORD': os.getenv('PASS', ''), 
     }
 }
 
