2e5194340f79ff856b8757ca2f667c08aff576ea
Israel Blancas <iblancasa@gmail.com>
2015-02-21 10:42:28 +0100

    Añadiendo acceso a variables de la BD

---
+++ wsgi/bd.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os
+
+BDNAME=os.environ['BDNAME']
+BDHOST=os.environ['OPENSHIFT_MYSQL_DB_HOST']+':'+os.environ['OPENSHIFT_MYSQL_DB_PORT']
+BDPASS=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+BDUSER=os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
